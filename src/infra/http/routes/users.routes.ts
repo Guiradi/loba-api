6dfe4ba0908fca784cb83bf614afb34374d1d285
Guilherme Ponsoni Ferreira <guiponsoni@gmail.com>
2022-08-10 22:04:09 -0300

    Comenta middleware

--- src/infra/http/routes/users.routes.ts
+++ src/infra/http/routes/users.routes.ts
@@ -1,12 +1,12 @@
-import { adaptMiddleware } from '@core/infra/adapters/ExpressMiddlewareAdapter'
+// import { adaptMiddleware } from '@core/infra/adapters/ExpressMiddlewareAdapter'
 import { adaptRoute } from '@core/infra/adapters/ExpressRouteAdapter'
 import { Router } from 'express'
 import { makeCreateUserController } from '../factories/controllers/CreateUserControllerFactory'
-import { makeEnsureAuthenticatedMiddleware } from '../factories/middlewares/EnsureAuthenticatedMiddlewareFactory'
+// import { makeEnsureAuthenticatedMiddleware } from '../factories/middlewares/EnsureAuthenticatedMiddlewareFactory'
 
 const usersRouter = Router()
 
-usersRouter.use(adaptMiddleware(makeEnsureAuthenticatedMiddleware()))
+// usersRouter.use(adaptMiddleware(makeEnsureAuthenticatedMiddleware()))
 
 usersRouter.post('/', adaptRoute(makeCreateUserController()))
 
